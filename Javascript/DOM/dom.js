36595e2cd4ede25511d4b211a1b5a678d9c9c54c
boinaohemam <boinaohemam@gmail.com>
2025-10-19 23:19:22 +0530

    element manipulation

--- Javascript/DOM/dom.js
+++ Javascript/DOM/dom.js
@@ -6,3 +6,7 @@ for (div of divs){
     div.innerText = `new unique box ${idx}`;
     idx++;
 }
+
+let heading = document.createElement("h1");
+heading.innerText = "this is a test heading";
+document.querySelector("div").append(heading);
